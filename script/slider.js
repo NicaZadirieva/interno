e2471dfd5227a54d7fbf01bb2e4b7d7652a3c0a3
Veronica <no.demchenko@yandex.ru>
2024-12-03 16:37:58 +0300

    feat(slider): create slider
    slider from swiper.js

--- script/slider.js
+++ script/slider.js
@@ -1,6 +1,11 @@
 function initializeSwiper(swiperClass) {
   var swiper = new Swiper(swiperClass, {
     loop: true,
+    effect: "coverflow",
+    coverFlowEffect: {
+      rotate: 30,
+      slideShadows: false,
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
